223c712396f7a442288b434490064aee7a6caf3d
Admin <Admin@TEB123003>
2023-01-25 11:30:37 +0100

    Add project files.

---
+++ uppgift 5.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift5_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = new string[4];
+            text[0] = "yep";
+            text[1] = "yes";
+            text[2] = "yee";
+            text[3] = "yeee";
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(text[i]);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
